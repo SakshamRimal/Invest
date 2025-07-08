fadb96f19c9bb0733504808a6425c95bc64eafca
Deeptibhatta27 <deeptibhatta2003@gmail.com>
2025-07-08 17:33:30 +0545

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import (
+    protected_view,
+    AdminOnlyView,
+    InvestorRegisterView,
+    StartupRegisterView,
+    InvestorDashboardView,
+    StartupDashboardView,
+    home,
+)
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('protected/', protected_view, name='protected'),
+    path('admin-only/', AdminOnlyView.as_view(), name='admin_only'),
+
+    # Registration endpoints
+    path('register/investor/', InvestorRegisterView.as_view(), name='register_investor'),
+    path('register/startup/', StartupRegisterView.as_view(), name='register_startup'),
+
+    # Dashboard endpoints
+    path('investor-dashboard/', InvestorDashboardView.as_view(), name='investor_dashboard'),
+    path('startup-dashboard/', StartupDashboardView.as_view(), name='startup_dashboard'),
+
+    # Public homepage
+    path('', home, name='home'),
+]
